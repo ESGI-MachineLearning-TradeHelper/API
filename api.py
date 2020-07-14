e7a9e95ddc89f84b0e4e66ce5750e0aa28aa5eb6
DELMER <d.pierrebox@gmail.com>
2020-07-14 21:42:29 +0200

    Adding switch

--- api.py
+++ api.py
@@ -5,9 +5,11 @@ import jsonpickle
 app = Flask("DEEPLEARNING")
 app.config["DEBUG"] = True
 
+
 @app.route('/api/predict', methods=['POST'])
 def predict():
     r = request
+    type = r.args.get("type")
     if not r.files:
         return Response(status=400)
     elif not r.files['image']:
@@ -15,11 +17,21 @@ def predict():
     else:
         file = r.files['image']
         img = Image.open(file.stream)
-        img = img.save("img/"+file.filename)
-        #CALL FOR MODEL
-
-        #RETURN OF MODEL
+        img = img.save("img/" + file.filename)
+        # CALL FOR MODEL
+        if type == 1:
+            print("MODEL 1")
+        elif type == 2:
+            print("MODEL 2")
+        elif type == 3:
+            print("MODEL 3")
+        elif type == 4:
+            print("MODEL 4")
+        else:
+            print("MODEL 5")
+        # RETURN OF MODEL
         response_pickled = jsonpickle.encode({'msg': 'success'})
         return Response(response=response_pickled, status=200, mimetype="application/json")
+
 
 app.run(host="0.0.0.0", port=5000)
