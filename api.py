b7f8df97c10e02bb37ed48720821ebbc3daad60b
Joris Demandre <joris.demandre@agence-soon.fr>
2020-07-16 18:55:43 +0200

    retrieve convert and save base64image from request data and return sample

--- api.py
+++ api.py
@@ -1,23 +1,28 @@
 from flask import Flask, request, Response
 from PIL import Image
 import jsonpickle
+import base64
 
 app = Flask("DEEPLEARNING")
 app.config["DEBUG"] = True
 
-
 @app.route('/api/predict', methods=['POST'])
 def predict():
     r = request
+
     type = r.args.get("type")
-    if not r.files:
+    if not r.form:
         return Response(status=400)
-    elif not r.files['image']:
+    form = r.form.to_dict()
+    if not form['image[content]']:
         return Response(status=400)
     else:
-        file = r.files['image']
-        img = Image.open(file.stream)
-        img = img.save("img/" + file.filename)
+        stripped_base64image = form['image[content]'].split(',')[1]
+        img_data = base64.b64decode(stripped_base64image)
+        filename = "img/" + form['image[name]']
+        with open(filename, 'wb') as f:
+            f.write(img_data)
+
         # CALL FOR MODEL
         if type == 1:
             print("MODEL 1")
@@ -30,7 +35,7 @@ def predict():
         else:
             print("MODEL 5")
         # RETURN OF MODEL
-        response_pickled = jsonpickle.encode({'msg': 'success'})
+        response_pickled = jsonpickle.encode({'predict': 'triangle','predict_percent':30})
         return Response(response=response_pickled, status=200, mimetype="application/json")
 
 
